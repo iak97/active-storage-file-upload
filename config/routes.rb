1dc00639beabb70359235f8f34522887d5e8bea6
iak97 <psajithkumar97@gmail.com>
2023-07-08 17:51:48 +0530

    users crud with profile picture

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "users#index"
+
+  resources :users
+  resources :users do
+    delete :delete_profile_picture
+  end
 end
